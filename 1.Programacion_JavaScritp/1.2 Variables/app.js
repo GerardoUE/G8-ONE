49d0209b0b51fb67c4fa7c7a88a5ec68176d5991
GerardoUE <gerardue@gmail.com>
2025-01-16 16:38:08 -0500

    change files

---
+++ 1.Programacion_JavaScritp/1.2 Variables/app.js
@@ -0,0 +1,6 @@
+alert('Hola Mundo');
+prompt('Me indicas un numero');
+
+let = 'Tipo de variable modificable';
+const = 'Tipo de variable no modificable';
+var = 'Tipo de variable odificable que ya nlo se usa'
