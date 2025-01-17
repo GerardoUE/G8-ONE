091fa3e9e12598a0387f228b797df926ed82ddea
GerardoUE <gerardue@gmail.com>
2025-01-16 23:33:47 -0500

    add file

--- 1.Programacion_JavaScritp/1.2 Variables/app.js
+++
@@ -1,6 +0,0 @@
-alert('Hola Mundo');
-prompt('Me indicas un numero');
-
-let = 'Tipo de variable modificable';
-const = 'Tipo de variable no modificable';
-var = 'Tipo de variable odificable que ya nlo se usa'
---
+++ 1.Programacion_JavaScritp/1.2-Variables/app.js
@@ -0,0 +1,9 @@
+alert('Hola Mundo'); //Nos parecer un mensaje de alerta en el navegador
+let numUser = prompt('Me indicas un numero'); //Nos aparece un mensaje de ingresar un dato en el navegador
+/*
+let 'Tipo de variable modificable';
+const 'Tipo de variable no modificable';
+var 'Tipo de variable odificable que ya nso se usa'
+*/
+//Camel Case : es el nombre que se le da al nombre de variable que se usa de esta manera nombreUsuario cuando empieza la segunda plabra pegado en mayuscula.
+
