40d7dedee932570c63efe2eb0c5abf9e14ac610a
GerardoUE <gerardue@gmail.com>
2025-01-17 00:45:56 -0500

    add files

--- 1.Programacion_JavaScritp/1.2-Variables/app.js
+++ 1.Programacion_JavaScritp/1.2-Variables/app.js
@@ -1,9 +1,23 @@
-alert('Hola Mundo'); //Nos parecer un mensaje de alerta en el navegador
-let numUser = prompt('Me indicas un numero'); //Nos aparece un mensaje de ingresar un dato en el navegador
+//alert('Hola Mundo'); //Nos parecer un mensaje de alerta en el navegador
 /*
+VARIABLES
 let 'Tipo de variable modificable';
 const 'Tipo de variable no modificable';
 var 'Tipo de variable odificable que ya nso se usa'
-*/
-//Camel Case : es el nombre que se le da al nombre de variable que se usa de esta manera nombreUsuario cuando empieza la segunda plabra pegado en mayuscula.
 
+numUser es el nombre de la variable donde guardamos datos (puede llamar asi o como quieres, la funcion de una variable es guardar, almacenar o reservar datos que se usaran despues)
+
+Camel Case : es el nombre que se le da al nombre de variable que se usa de esta manera nombreUsuario 
+cuando empieza la segunda plabra pegado en mayuscula.
+*/
+let numSecreto = 6;
+let numUser = prompt('Me indicas un numero'); // con prompt Nos aparece un mensaje de ingresar un dato en el navegador, ese dato ingresado se guarda en la variable numUser
+
+console.log(numUser); //Nos aparece el dto ingresado en la consola del navegador que se muestra presionando F12 , a eso se le lama imprimir y se ve en otros lenguajes mayor mente con print
+
+if (numSecreto == numUser){
+  alert('Acertaste el numero 😁')
+}
+else{
+  alert('No acertaste el numero 😔')
+};
